0381bd4f494b2a40d93de7d11a5dcdec7166d486
ChristianSurbatovich <csfresno@mail.fresnostate.edu>
2019-02-22 15:48:53 -0800

    Update MessageValues.cs

--- Assets/Scripts/Networking/MessageValues.cs
+++ Assets/Scripts/Networking/MessageValues.cs
@@ -21,11 +21,8 @@ namespace ShipGame.Network
         public const byte FEED = 0x0D;
         public const byte EXPLODE = 0x0E;
         public const byte POSITION = 0x0F;
-        public const byte STAT = 0x10;
-        public const byte ABILITY = 0x11;
-        public const byte ADD_ACTION = 0x12;    
+        public const byte STAT = 0x10;   
         public const byte EQUIP_ITEM = 0x13; 
-        public const byte ITEM = 0x14;
         public const byte LOOT_AREA = 0x15;
         public const byte LOOT_ITEM = 0x16;
         public const byte ADD_LOOT_ITEM = 0x17;
@@ -34,15 +31,21 @@ namespace ShipGame.Network
         public const byte SYNC = 0x1A;
         public const byte DESTRUCTION_STATE_RESET = 0x1B;
         public const byte DESTRUCTION_STATE = 0x1C;
-        public const byte ADD_ABILITY = 0x1D;
-        public const byte EQUIP_ABILITY = 0x1E;
-        public const byte REMOVE_ABILITY = 0x1F;
-        public const byte UNEQUIP_ABILITY = 0x20;
-        public const byte POSITION_FULL = 0x21;
-        public const byte VELOCITY = 0x22;
-        public const byte VELOCITY_FULL = 0x23;
-        public const byte AGENT_SYNC = 0x24;
-        public const byte ADD_AGENT = 0x25;
+        public const byte ADD_ITEM = 0x1D;
+        public const byte USE_ITEM = 0x1E;
+        public const byte ADD_STATIC_OBJECT = 0x1F;
+        public const byte ADD_DYNAMIC_OBJECT = 0x20;
+        public const byte ADD_STATIC_ACTOR = 0x21;
+        public const byte ADD_DYNAMIC_ACTOR = 0x22;
+        public const byte MOVE_ITEM = 0x23;
+        public const byte REMOVE_ITEM = 0x24;
+        public const byte REMOVE_LOOT_ITEM = 0x25;
+        public const byte POSITION_FULL = 0x26;
+        public const byte VELOCITY = 0x27;
+        public const byte VELOCITY_FULL = 0x28;
+        public const byte AGENT_SYNC = 0x29;
+        public const byte ADD_AGENT = 0x2A;
+        public const byte FORCE_ITEM_USE = 0x2B;
         public const byte TEST = 0xFF;
     }
 
